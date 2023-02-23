8db8df42e22dc76d5144609f99d49a1fce229261
Jan Nils Ferner <contact@jnferner.com>
2023-02-23 19:33:14 +0100

    Optimize performance

--- src/prepare.rs
+++ src/prepare.rs
@@ -8,13 +8,8 @@ use bevy::render::render_resource::{
 };
 use bevy::render::renderer::RenderDevice;
 
-pub fn prepare_instance_buffers(
-    pipeline: Res<GrassPipeline>,
-    mut cache: ResMut<GrassCache>,
-    region_config: Res<RegionConfig>,
-    render_device: Res<RenderDevice>,
-) {
-    if !cache.is_changed() && !region_config.is_changed() {
+pub fn prepare_uniform_buffers(mut cache: ResMut<GrassCache>, render_device: Res<RenderDevice>) {
+    if !cache.is_changed() {
         return;
     }
     for instance_data in cache.values_mut() {
@@ -23,6 +18,20 @@ pub fn prepare_instance_buffers(
             contents: bytemuck::cast_slice(&instance_data.grass.instances.as_slice()),
             usage: BufferUsages::VERTEX | BufferUsages::COPY_DST,
         });
+        instance_data.buffer = Some(entity_buffer);
+    }
+}
+
+pub fn prepare_instance_buffer(
+    pipeline: Res<GrassPipeline>,
+    mut cache: ResMut<GrassCache>,
+    region_config: Res<RegionConfig>,
+    render_device: Res<RenderDevice>,
+) {
+    if !region_config.is_changed() {
+        return;
+    }
+    for instance_data in cache.values_mut() {
         let color_buffer = render_device.create_buffer_with_data(&BufferInitDescriptor {
             label: Some("Color buffer"),
             contents: bytemuck::cast_slice(&region_config.color.as_rgba_f32()),
@@ -45,7 +54,6 @@ pub fn prepare_instance_buffers(
             ],
         };
         let bind_group = render_device.create_bind_group(&bind_group_descriptor);
-        instance_data.buffer = Some(entity_buffer);
         instance_data.uniform_bind_ground = Some(bind_group);
     }
 }
